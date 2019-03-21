0e15ce247ab27351b3cfd56026f85e4dfb44de02
Michael <kipsang20323@gmail.com>
2019-03-20 23:01:54 -0600

    partially working drag-drop

--- client/src/DragText.js
+++ client/src/DragText.js
@@ -10,7 +10,9 @@ export default class DragText extends Component {
 			droppedText: "",
 			startedDrag: false,
 			makeInvisible: false,
-			hovering: false
+			hovering: false,
+			textX: null,
+			textY: null
 		};
 
 		this.textRef = React.createRef();
@@ -22,15 +24,25 @@ export default class DragText extends Component {
 		});
 	};
 
+	// https://developer.mozilla.org/en-US/docs/Web/CSS/CSSOM_View/Coordinate_systems#Example
 	onDrag = (e, text) => {
-		console.log("regular onDrag evt: ", e, text);
+		console.log("onDrag clientX/Y", e.clientX, e.clientY);
+		console.log("onDrag screenX/Y", e.screenX, e.screenY);
+		// console.log("onDrag movementX/Y", e.movementX, e.movementY); //difference btwn last mouse movement and next
+		console.log("onDrag pageX/Y", e.pageX, e.pageY);
+
+		// console.log("regular onDrag evt: ", e, text);
+		this.setState({
+			textX: e.clientX,
+			textY: e.clientY
+		});
 	};
 
 	onDragStart = (e) => {
-		console.log("dragStart evt target: ", e.target);
+		// console.log("dragStart evt target: ", e.target);
 		// console.log("dragStart innerHTML: ", e.target.innerHTML);
 		// console.log("dragStart textContent: ", e.target.textContent);
-		console.log("dragStart dataTransfer: ", e.dataTransfer);
+		// console.log("dragStart dataTransfer: ", e.dataTransfer);
 		let typedText = e.target.textContent;
 		e.dataTransfer.setData("text", typedText);
 		this.setState({
@@ -43,9 +55,15 @@ export default class DragText extends Component {
 
 	onDragEnd = (e) => {
 		console.log("dragEnd evt: ", e);
+		console.log("dragEnd offset: ", e.target.style);
+		const x = e.clientX;
+		const y = e.clientY;
+		console.log("textRef styles: ", this.textRef.style);
 		this.setState({
 			startedDrag: false,
-			makeInvisible: false
+			makeInvisible: false,
+			textX: e.clientX,
+			textY: e.clientY
 		});
 	};
 
@@ -56,12 +74,12 @@ export default class DragText extends Component {
 
 	onDragEnter = (e) => {
 		e.preventDefault();
-		console.log("dragEnter evt: ", e);
+		// console.log("dragEnter evt: ", e);
 		this.setState({ hovering: true });
 	};
 
 	onDragLeave = (e) => {
-		console.log("dragLeave evt: ", e);
+		// console.log("dragLeave evt: ", e);
 		//set hovering class to false on individual box
 	};
 
@@ -76,8 +94,19 @@ export default class DragText extends Component {
 	};
 
 	render() {
+		const styles = {
+			// clientX: this.state.textX,
+			// clientY: this.state.textY,
+			// position: "absolute",
+			top: `${this.state.textY}px`,
+			left: `${this.state.textX}px`
+		};
+
 		return (
 			<div className="drag-container">
+				<h6>
+					currentX: {this.state.textX}, currentY: {this.state.textY}
+				</h6>
 				<img
 					className="img-drop"
 					id="data"
@@ -100,6 +129,7 @@ export default class DragText extends Component {
 						? "invisible"
 						: ""}`}
 					ref={this.textRef}
+					style={{ top: `${this.state.textY}px`, left: `${this.state.textX}px`, zIndex: "100" }}
 					onDrag={(e) => this.onDrag(e, e.target.innerHTML)}
 					onDragStart={(e) => this.onDragStart(e)}
 					onDragEnd={(e) => this.onDragEnd(e)}
